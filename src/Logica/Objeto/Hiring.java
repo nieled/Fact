b8e546550ed83ebd51e99909fc65bf4f4959bce5
Eduardo Calle <dcallel@est.ups.edu.ec>
2016-10-14 11:55:22 -0500

    Create business logic's objects

---
+++ src/Logica/Objeto/Hiring.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica.Objeto;
+
+/**
+ * Contrataciones
+ * @author niel
+ */
+public class Hiring {
+    public boolean createEmployee (){
+        return false;
+    }
+    
+    public boolean deleteEmployee(){
+        return false;
+    }
+}
