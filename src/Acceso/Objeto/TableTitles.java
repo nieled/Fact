ca50573ba76e737dace00d7b7b687438fd7b8e5f
Eduardo Calle <dcallel@est.ups.edu.ec>
2016-10-14 12:39:50 -0500

    Add Select titles method

--- src/Acceso/Objeto/TableTitles.java
+++ src/Acceso/Objeto/TableTitles.java
@@ -6,15 +6,28 @@
 package Acceso.Objeto;
 
 import Acceso.Entidad.MasterTitle;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Objeto de Acceso a datos para tabla Titles
+ *
  * @author niel
  */
 public class TableTitles {
 
-    MasterTitle selectTitles(int emp_no) {
-        return null;
+    public List<MasterTitle> selectTitles(int emp_no) throws SQLException {
+        List<MasterTitle> ans = new ArrayList<MasterTitle>();
+        ResultSet rs = Resources.Conexion.getQuery("SELECT * FROM TITLES WHERE EMP_NO=" + emp_no);
+        while (rs.next()) {
+            int empNo = rs.getInt(1);
+            String title = rs.getString(1);
+            ans.add(new MasterTitle(empNo, title));
+        }
+        return ans;
     }
 
     boolean insertTitle(MasterTitle mt) {
