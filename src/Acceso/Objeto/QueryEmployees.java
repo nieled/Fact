f1e3adee0de82650526191d451b62b291e7093a5
Eduardo Calle <nieled@riseup.net>
2016-10-17 12:47:20 -0500

    Populate searchByID method

--- src/Acceso/Objeto/QueryEmployees.java
+++ src/Acceso/Objeto/QueryEmployees.java
@@ -6,17 +6,25 @@
 package Acceso.Objeto;
 
 import Acceso.Entidad.ListaEmpleados;
+import Resources.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Objeto de Acceso a datos para querys de Empleados
  * @author niel
  */
 public class QueryEmployees {
-    ListaEmpleados searchByID(int id) {
-        return null;
+    public ListaEmpleados searchByID(int id) throws SQLException {
+        PreparedStatement pst = Conexion.prepStmt("SELECT * FROM public.employees, public.titles WHERE public.employees.emp_no=public.titles.emp_no;");
+        ResultSet rs = pst.executeQuery();
+        rs.next();
+        ListaEmpleados le = new ListaEmpleados(rs.getInt(1), rs.getDate(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getDate(6), rs.getString(8));
+        return le;
     }
     
-    ListaEmpleados searchByName(String name){
+    public ListaEmpleados searchByName(String name){
         return null;
     }
 }
