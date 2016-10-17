d143bd684f2d7a853e55d492cdcd023d964771c9
Eduardo Calle <nieled@riseup.net>
2016-10-17 12:31:32 -0500

    Add static method prepStmt to get a PreparedStatement object to execute querys.

--- src/Resources/Conexion.java
+++ src/Resources/Conexion.java
@@ -7,6 +7,7 @@ package Resources;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -74,10 +75,14 @@ public class Conexion {
     
     public static boolean doQuery(String query) throws SQLException {
         Statement st = conn.createStatement();
-        System.out.println("Executing insert: " + query);
         st.executeUpdate(query);
         st.close();
         return true;
+    }
+    
+    public static PreparedStatement prepStmt(String query) throws SQLException {
+        PreparedStatement pst = conn.prepareStatement(query);
+        return pst;
     }
 
     public Connection getConn() {
